9fe64a693e59e95814f3d526ae89118489c3d884
xh <2715782947@qq.com>
2021-05-26 01:15:32 +0800

    注释说明angular-cli文件

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
+  selector: 'app-root', // 使用这个组件的名称
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angular-test';
+  title = 'angular-test';  // 定义属性
+
+  constructor(){  // 构造函数
+
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,25 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+// 这个文件是 angular 根模块，告诉angular如何组装应用
+import { NgModule } from '@angular/core';  // Angular的核心模块
+import { BrowserModule } from '@angular/platform-browser';  // BrowserModule 浏览器解析的模块
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NewsComponent } from './components/news/news.component';  // 根组件
 
+import { TestService } from "./service/test.service"
+
+// @NgModule 装饰器，@NgModule接受一个元数据对象，告诉 Angular 如何编译和启动应用
 @NgModule({
-  declarations: [
-    AppComponent
+  declarations: [   // 配置当前项目运行的组件    (组件) 
+    AppComponent, NewsComponent
   ],
-  imports: [
+  imports: [   // 配置当前模块运行依赖的其他模块    (模块)
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [TestService],  // 配置项目所需要的服务   (服务)
+  bootstrap: [AppComponent] // 指定应用的主视图(称为根组件) 通过引导跟AppModule来启动应用 ， 这里一般写的是根组件
 })
+
+// 根模块不需要导出任何东西  ， 因为其他组件不需要导入根模块
 export class AppModule { }
